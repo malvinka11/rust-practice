5c8e2fcc1ee221392febeeb8962075c78b3453e7
malvinka11 <viktoria.matvienko@e-u.edu.ua>
2025-04-04 12:04:18 +0300

    Update hw03.rs

---
+++ src/homework/hw03.rs
@@ -0,0 +1,17 @@
+fn main() {
+    const WIDTH: usize = 20; 
+    const HEIGHT: usize = 13; 
+
+    for i in 0..HEIGHT {
+        for j in 0..WIDTH {
+            if i == 0 || i == HEIGHT - 1 {
+                print!("*");
+            } else if j == i || j == WIDTH - i - 1 {
+                print!("*");
+            } else {
+                print!(" ");
+            }
+        }
+        println!();
+    }
+}
